823845b3aff6d8927eec7528504d865f92b799d2
Hyemin-12 <w2112@e-mirim.hs.kr>
2022-08-30 19:08:40 +0900

    DeliveryListFragment 구현

---
+++ KokKok/app/src/main/java/com/example/kokkok/DeliveryAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.kokkok
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class DeliveryAdapter(): RecyclerView.Adapter<DeliveryAdapter.MyViewHolder>(){
+
+    // 임시 cardView
+    var titles = arrayOf("배달1", "배달2", "배달3", "배달4", "배달5")
+    var details = arrayOf("Item one", "Item two", "Item three", "Item four", "Itme five")
+    var images = intArrayOf(
+        R.drawable.cat,
+        R.drawable.cat,
+        R.drawable.cat,
+        R.drawable.cat,
+        R.drawable.cat
+    )
+
+
+    class MyViewHolder(itemview: View) : RecyclerView.ViewHolder(itemview) {
+        val itemimage: ImageView = itemview.findViewById(R.id.item_image)
+        val itemtitle: TextView = itemview.findViewById(R.id.item_title)
+        val itemdetail: TextView = itemview.findViewById(R.id.item_detail)
+    }
+
+    override fun onCreateViewHolder(viewgroup: ViewGroup, position: Int): MyViewHolder {
+        var v: View = LayoutInflater.from(viewgroup.context).inflate(R.layout.card_layout, viewgroup, false)
+
+        return MyViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        // cardView 구성
+        holder.itemtitle.setText(titles.get(position))
+        holder.itemimage.setImageResource(images.get(position))
+        holder.itemdetail.setText(details.get(position))
+
+        holder.itemView.setOnClickListener {
+            // 프로젝트 세부 사항 페이지로 이동
+            val intent = Intent(holder.itemView.context, GiftInfoActivity::class.java)
+            holder.itemView.context.startActivity(intent)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return titles.size
+    }
+}
--- KokKok/app/src/main/java/com/example/kokkok/DeliveryListFragment.kt
+++ KokKok/app/src/main/java/com/example/kokkok/DeliveryListFragment.kt
@@ -8,18 +8,18 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class GiftListFragment : Fragment() {
+class DeliveryListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.gift_list_fragment, container, false)
+        val view = inflater.inflate(R.layout.delivery_list_fragment, container, false)
 
         // recyclerView -> cardView를 목록으로 보여줌
-        var recyclerView = view.findViewById<RecyclerView>(R.id.gift_recyclerview) // recyclerview id
+        var recyclerView = view.findViewById<RecyclerView>(R.id.delivery_recyclerview) // recyclerview id
         var layoutManager = LinearLayoutManager(activity)
         recyclerView.layoutManager = layoutManager
-        var adapter = GiftAdapter()
+        var adapter = DeliveryAdapter()
         recyclerView.adapter = adapter
 
         return view
