48cc43865d02e8683ac2d95fa23c7472bf233806
Hyemin-12 <w2112@e-mirim.hs.kr>
2022-10-13 18:44:24 +0900

    MyPageFragment 오류 수정

--- app/src/main/java/com/example/kokkok/MyPageFragment.kt
+++ app/src/main/java/com/example/kokkok/MyPageFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 
 
 class MyPageFragment : Fragment() {
@@ -17,21 +18,27 @@ class MyPageFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.my_page_fragment, container, false)
 
+        val card1 = view.findViewById<CardView>(R.id.card_view1)
+        val card2 = view.findViewById<CardView>(R.id.card_view2)
+        val card3 = view.findViewById<CardView>(R.id.card_view3)
 
-        val itemimage1 = view.findViewById<ImageView>(R.id.item_image1)
-        val itemtitle1 = view.findViewById<TextView>(R.id.item_title1)
-        val itemdetail1 = view.findViewById<TextView>(R.id.item_detail1)
-
-        val itemtitle2 = view.findViewById<TextView>(R.id.item_title2)
-        val itemdetail2 = view.findViewById<TextView>(R.id.item_detail2)
-        // 마이페이지 구현
-
-        view.setOnClickListener {
+        card1.setOnClickListener {
             // 프로젝트 세부 사항 페이지로 이동
             val intent = Intent(view.context, GiftInfoActivity::class.java)
             view.context.startActivity(intent)
         }
-        // 마이페이지 구현
+
+        card2.setOnClickListener {
+            // 프로젝트 세부 사항 페이지로 이동
+            val intent = Intent(view.context, GiftInfoActivity::class.java)
+            view.context.startActivity(intent)
+        }
+
+        card3.setOnClickListener {
+            // 프로젝트 세부 사항 페이지로 이동
+            val intent = Intent(view.context, GiftInfoActivity::class.java)
+            view.context.startActivity(intent)
+        }
 
         return view
     }
