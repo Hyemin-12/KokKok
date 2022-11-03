99c241285815f48fb2ec9f0cb4e9a71784d7145d
youjin <w2117@e-mirim.hs.kr>
2022-11-03 19:29:11 +0900

    login & signup 안드로이드 구현 완료

--- app/src/main/java/com/example/kokkok/MainActivity.kt
+++ app/src/main/java/com/example/kokkok/MainActivity.kt
@@ -3,16 +3,101 @@ package com.example.kokkok
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import com.example.kokkok.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    val api = RetroInterface.create()
+    lateinit var allUser : ArrayList<User>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // 상단바 숨기기
-        supportActionBar?.hide()
+        api.allUser().enqueue(object: retrofit2.Callback<ArrayList<User>>{
+            override fun onResponse(
+                call: Call<ArrayList<User>>,
+                response: Response<ArrayList<User>>
+            ) {
+                allUser = response.body()?:return
+            }
 
-        val intent = Intent(this, AddGiftActivity::class.java)
-        startActivity(intent)
+            override fun onFailure(call: Call<ArrayList<User>>, t: Throwable) {
+
+            }
+        })
+        binding.toSignInBtn.setOnClickListener {
+//            binding.apply {
+//                val id = emailInput.text.toString()
+//                val pw = passwordInput.text.toString()
+//
+//                if(id == "" || pw == "") {
+//                    Toast.makeText(applicationContext, "입력하지 않은 정보가 있습니다.", Toast.LENGTH_SHORT).show()
+//                    return@setOnClickListener
+//                }
+//            }
+//            val newUser = RegisterModel(binding.emailInput.text.toString(), binding.passwordInput.text.toString())
+//            api.register(newUser).enqueue(object: retrofit2.Callback<RegisterResult>{
+//                override fun onResponse(call: Call<RegisterResult>, response: Response<RegisterResult>) {
+//                    val result = response.body()?.message ?: return
+//                    if(result)
+//                        Toast.makeText(applicationContext, "회원가입 성공", Toast.LENGTH_SHORT).show()
+//                    else
+//                        Toast.makeText(applicationContext, "회원가입 실패, 이미 존재하는 아이디 입니다.", Toast.LENGTH_SHORT).show()
+//                }
+//
+//                override fun onFailure(call: Call<RegisterResult>, t: Throwable) {
+//                    Log.d("testt", t.message.toString())
+//                }
+//            })
+            val intent = Intent(this@MainActivity, SignupActivity::class.java)
+            intent.putExtra("userList", allUser)
+            startActivity(intent)
+        }
+        binding.loginBtn.setOnClickListener {
+            binding.apply {
+                val id = emailInput.text.toString()
+                val pw = passwordInput.text.toString()
+
+                if(id == "" || pw == "") {
+                    Toast.makeText(applicationContext, "입력하지 않은 정보가 있습니다.", Toast.LENGTH_SHORT).show()
+                    return@setOnClickListener
+                }
+            }
+            val loginUser = LoginModel(binding.emailInput.text.toString(), binding.passwordInput.text.toString())
+            api.login(loginUser).enqueue(object: Callback<LoginResult> {
+                override fun onResponse(call: Call<LoginResult>, response: Response<LoginResult>) {
+                    val user_uid = response.body()?.UID ?: return
+                    if(user_uid != -1) {
+                        Toast.makeText(applicationContext, "로그인 성공", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this@MainActivity, NaviActivity::class.java)
+                        intent.putExtra("userList", allUser)
+                        startActivity(intent)
+
+                        val sharedPreferences = getSharedPreferences("user_info", Context.MODE_PRIVATE)
+                        val editor = sharedPreferences.edit()
+                        editor.putString("uid", user_uid.toString())
+                        editor.apply()
+
+                        Log.d("testt", user_uid.toString())
+                    }
+                    else{
+                        Toast.makeText(applicationContext, "로그인 실패, 아이디 또는 비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+
+                override fun onFailure(call: Call<LoginResult>, t: Throwable) {
+                    Log.d("testt", t.message.toString())
+                }
+            })
+        }
+
     }
 }
--- app/src/main/java/com/example/kokkok/SignupActivity.kt
+++ app/src/main/java/com/example/kokkok/SignupActivity.kt
@@ -1,11 +1,79 @@
 package com.example.kokkok
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import com.example.kokkok.databinding.ActivityMainBinding
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
+import retrofit2.Call
+import retrofit2.Response
 
 class SignupActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    val api = RetroInterface.create()
+    lateinit var allUser : ArrayList<User>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.signup_activity)
+
+        val completeBtn : Button = findViewById(R.id.complete_btn)
+        val nameInput : TextView = findViewById(R.id.name_input)
+        val emailInput : TextView = findViewById(R.id.email_input)
+        val pwInput : TextView = findViewById(R.id.pw_input)
+        val checkPw : TextView = findViewById(R.id.check_pw)
+
+
+        api.allUser().enqueue(object: retrofit2.Callback<ArrayList<User>>{
+            override fun onResponse(
+                call: Call<ArrayList<User>>,
+                response: Response<ArrayList<User>>
+            ) {
+                allUser = response.body()?:return
+            }
+
+            override fun onFailure(call: Call<ArrayList<User>>, t: Throwable) {
+
+            }
+        })
+
+        completeBtn.setOnClickListener{
+            binding.apply {
+                val name = nameInput.text.toString()
+                val id = emailInput.text.toString()
+                val pw = pwInput.text.toString()
+                val checkpw = checkPw.text.toString()
+
+                if(id == "" || pw == "" || name =="" || checkpw == "") {
+                    Toast.makeText(applicationContext, "입력하지 않은 정보가 있습니다.", Toast.LENGTH_SHORT).show()
+                    return@setOnClickListener
+                }
+                if(pw != checkpw) {
+                    Toast.makeText(applicationContext, "비밀번호를 잘못 입력하셨습니다.", Toast.LENGTH_SHORT).show()
+                    return@setOnClickListener
+                }
+            }
+            val newUser = RegisterModel(emailInput.text.toString(),pwInput.text.toString())
+            api.register(newUser).enqueue(object: retrofit2.Callback<RegisterResult>{
+                override fun onResponse(call: Call<RegisterResult>, response: Response<RegisterResult>) {
+                    val result = response.body()?.message ?: return
+                    if(result)
+                        Toast.makeText(applicationContext, "회원가입 성공", Toast.LENGTH_SHORT).show()
+                    else
+                        Toast.makeText(applicationContext, "회원가입 실패, 이미 존재하는 아이디 입니다.", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onFailure(call: Call<RegisterResult>, t: Throwable) {
+                    Log.d("testt", t.message.toString())
+                }
+            })
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("userList", allUser)
+            startActivity(intent)
+        }
     }
 }
